72532ff24ccec5bc7c77519e396685f9b6b4b48c
Horlarwarler <dcrescentsol@gmail.com>
2022-03-13 11:32:37 +0000

    test

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Determine if a random number is positive, negative or zero
+* Return: 0 on success
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+putchar('\n');
+return (0);
+}
