ebd7605927920a99306fe792d3f80e7700977a6b
Horlarwarler <dcrescentsol@gmail.com>
2022-03-14 10:13:08 +0000

    Done eith 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Determine if a random number is positive, negative or zero
+* Return: 0 on success
+*/
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+if (i < 9)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+}
