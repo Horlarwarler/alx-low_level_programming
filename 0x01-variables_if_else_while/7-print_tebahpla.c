9f3d3796d29c6988884b9bf833b1e72b6ee99cad
Horlarwarler <dcrescentsol@gmail.com>
2022-03-14 09:39:46 +0000

    Number 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Determine if a random number is positive, negative or zero
+* Return: 0 on success
+*/
+int main(void)
+{
+char c = 'z';
+while (c >= 'a')
+{
+putchar(c);
+c--;
+}
+putchar('\n');
+return (0);
+}
