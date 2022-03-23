ec45822d6301c6fac9de5030bb8793bb710769f4
Horlarwarler <dcrescentsol@gmail.com>
2022-03-23 06:24:25 +0000

    tets

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_islower - This file check if a character is in lower case
+ *@c: The parameter to check
+ *Return: 1 if the character is lower and 0 if it is bigger
+*/
+int _islower(char c)
+{
+	return (c >= 'a' && c <= 'z');
+}
