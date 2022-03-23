1c07ac9ad0efaec71bebfeef9fdb979852582d20
Horlarwarler <dcrescentsol@gmail.com>
2022-03-23 09:18:30 +0000

    test

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *jack_bauer - funtion name
+ *description: print out time
+ *Return: 0
+*/
+void jack_bauer(void)
+{
+	int hr = 0;
+
+	while (hr < 24)
+		int min = 59;
+
+			while (min > 0)
+				_putchar((hr / 10) + '0');
+				_putchar((hr % 10) + '0');
+				_putchar((":"));
+				_putchar((min / 10) + '0');
+				_putchar((min % 10) + '0');
+				min++;
+		hr++;
+	return (0);
+}
